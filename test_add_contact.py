3bc7aa3728ae4c0f3ec363128ad4960757c1e688
Anna Evdokimova <annaevd@gmail.com>
2021-07-21 03:16:08 +0300

    Создан класс для хранения свойств контакта. Класс использован для инициализации и передачи в методы тестовых данных.

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from contact import Contact
 
-class TestAddUser(unittest.TestCase):
+class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_user(self):
+    def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
-        self.open_users_page(wd)
-        self.create_user(wd)
+        self.login(wd, username='admin', password='secret')
+        self.open_contact_page(wd)
+        self.create_contact(wd, Contact(firstname='Anna', lastname='Evdokimova', address='Saint-Petersburg', home='+79211111111', email='test@test.ru'))
         self.return_to_home_page(wd)
         self.logout(wd)
 
@@ -22,29 +23,29 @@ class TestAddUser(unittest.TestCase):
     def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def create_user(self, wd):
-        # fill user form
+    def create_contact(self, wd, contact):
+        # fill contact form
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Anna")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Evdokimova")
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Saint-Petersburg")
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("+79211111111")
+        wd.find_element_by_name("home").send_keys(contact.home)
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("test@test.ru")
-        # submit user creation
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit contact creation
         wd.find_element_by_name("submit").click()
 
-    def open_users_page(self, wd):
+    def open_contact_page(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
