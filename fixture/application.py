536848033d6808c08d442cbc26d39a3089fa7942
Anna Evdokimova <annaevd@gmail.com>
2021-07-26 22:37:23 +0300

    Тест: Создание контакта переведен на py.test. Создана фикстура. Тестовые классы реализованы в виде функций. Фикстура передается в функции в качестве параметра.

--- fixture/application.py
+++ fixture/application.py
@@ -12,7 +12,33 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook/")
+
+    def return_to_home_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def create_contact(self, contact):
+        wd = self.wd
+        self.open_contact_page()
+        # fill contact form
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home)
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit contact creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_home_page()
+
+    def open_contact_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
 
     def destroy(self):
         self.wd.quit()
