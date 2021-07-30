5a49d9d5430482aeb63984ef72685c4ca4b09cf7
Anna Evdokimova <annaevd@gmail.com>
2021-07-31 01:33:46 +0300

    Добавлен тест: Редактирование имени первой группы

--- fixture/group.py
+++ fixture/group.py
@@ -31,8 +31,7 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
@@ -40,8 +39,7 @@ class GroupHelper:
     def edit_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # open group edit form
         wd.find_element_by_name("edit").click()
         # fill group edit form
@@ -55,3 +53,31 @@ class GroupHelper:
         # submit update
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+
+    def modify_first_group(self, new_group_data):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        # open group edit form
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(new_group_data)
+        # submit update
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
