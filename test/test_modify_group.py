ffd075340862b9f6652efff419d4d3d2be98ef0b
Anna Evdokimova <annaevd@gmail.com>
2021-08-10 00:55:32 +0300

    Реализована сортировка списков групп перед сравнением.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,16 +4,19 @@ from model.group import Group
 def test_modify_group_name(app):
     app.group.ensure_not_empty_groups_list(Group(name="First"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="ModifyName"))
+    group = Group(name="ModifyName")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-
-def test_modify_group_header(app):
-    app.group.ensure_not_empty_groups_list(Group(name="First"))
-    old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="ModifyHeader"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+# def test_modify_group_header(app):
+#     app.group.ensure_not_empty_groups_list(Group(name="First"))
+#     old_groups = app.group.get_group_list()
+#     app.group.modify_first_group(Group(header="ModifyHeader"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
 
