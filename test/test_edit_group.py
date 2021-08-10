86c2c0710367980ddf512425014036d0a7f30ec2
Anna Evdokimova <annaevd@gmail.com>
2021-08-10 23:43:10 +0300

    Реализована сортировка списков групп перед сравнением.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,5 +1,14 @@
 from model.group import Group
 
+
 def test_edit_group(app):
     app.group.ensure_not_empty_groups_list(Group(name="First"))
-    app.group.edit_first_group(Group(name="TestName_new", header="TestHeader_new", footer="TestFooter_new"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="TestName_new", header="TestHeader_new", footer="TestFooter_new")
+    group.id = old_groups[0].id
+    app.group.edit_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
