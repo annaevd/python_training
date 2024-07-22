2f2e330a564f4915bb034e9d0d31ef29c1b137de
annaevdokimova <annaevd@gmail.com>
2024-07-22 03:39:23 +0300

    Выполнена оптимизация ввода данных в формы для групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -5,3 +5,15 @@ def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="Update name group", header="Update header group", footer="Update footer group"))
     app.session.logout()
+
+
+def test_edit_first_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="Update name group"))
+    app.session.logout()
+
+
+def test_edit_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="Update header group"))
+    app.session.logout()
